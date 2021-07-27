3c879144d2a1a5f8d9a708867c0c75d772532748
lam-mac-20 <daoletunglam2002@gmail.com>
2021-07-27 15:47:57 +0700

    fix socket provider

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -19,6 +19,7 @@ export function SocketProvider({ children }) {
   const { user } = useUserContext();
   const userId = user._id;
   useEffect(() => {
+    if (!user) return;
     var newChatSocket = io(`${CONVERSATION_BASE_URL}/chat`, {
       query: {
         userId,
